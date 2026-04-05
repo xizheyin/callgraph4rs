cbf1fff004adc330e7fda08f125ed3b14d32713d
xizheyin <xizheyin@smail.nju.edu.cn>
2026-04-05 16:30:29 +0800

    fmt test

--- tests/resolution_module_test.rs
+++ tests/resolution_module_test.rs
@@ -1,6 +1,5 @@
 /// Integration test for resolution module functionality
 /// Tests function pointer and dynamic trait resolution
-
 mod common;
 
 use common::{run_call_cg4rs, unique_output_dir};
@@ -8,8 +7,7 @@ use std::path::PathBuf;
 
 #[test]
 fn test_resolution_module_integration() {
-    let manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("testdata/test1/Cargo.toml");
+    let manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("testdata/test1/Cargo.toml");
     let output_dir = unique_output_dir("cg4rs-resolution-test");
 
     // Run analysis with multiple find-callers targets
